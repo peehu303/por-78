4d68bdf920eb994605768a423eafe2c487272cb8
peehu303 <84406912+peehu303@users.noreply.github.com>
2021-07-29 14:04:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images = ["fam.jpeg","dadu.jpeg", "dadi.jpeg" , "papa.jpeg", "mama.jpeg", "bro.jpeg","me.jpeg"];
+var names = ["my family","my grandfather","my grandmother","my father","my mother","my brother","myself"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
